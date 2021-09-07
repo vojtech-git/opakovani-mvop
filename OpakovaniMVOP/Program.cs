bc5157ccac63de9090c4e5a4c76e380b2e8c729e
pejsar vojtech <pejsar.vojtech.2018@gmail.com>
2021-09-07 15:10:23 +0200

    Přidal jsem do Main metody test logiky

--- OpakovaniMVOP/Program.cs
+++ OpakovaniMVOP/Program.cs
@@ -10,6 +10,16 @@ namespace OpakovaniMVOP
     {
         static void Main(string[] args)
         {
+            Clovek adam = new Clovek();
+            Clovek david = new Clovek("david", "tlusty");
+
+            Obcan vojtech = new Obcan("000000/0000", "Vojtech", "Pejsar");
+
+            Console.WriteLine(adam.Jmeno + " " + adam.Prijmeni);
+            Console.WriteLine(david.Jmeno + " " + david.Prijmeni);
+            Console.WriteLine(vojtech.RodneCislo + " " + vojtech.Jmeno + " " + vojtech.Prijmeni);
+
+            Console.ReadLine();
         }
     }
 }
