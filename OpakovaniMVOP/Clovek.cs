5b9e5d0d4acb174ef7af6948d96b6b9eae063050
pejsar vojtech <pejsar.vojtech.2018@gmail.com>
2021-09-07 15:08:51 +0200

    Vytvořil jsem třídy clovek a občan a přiřadil jim jejich logiku

---
+++ OpakovaniMVOP/Clovek.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpakovaniMVOP
+{
+    class Clovek
+    {
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+
+        public Clovek()
+        {
+            Jmeno = "nezname";
+            Prijmeni = "nezname";
+        }
+
+        public Clovek(string _jmeno, string  _prijmeni)
+        {
+            Jmeno = _jmeno;
+            Prijmeni = _prijmeni;
+        }
+    }
+}
---
+++ OpakovaniMVOP/Obcan.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace OpakovaniMVOP
+{
+    class Obcan : Clovek
+    {
+        string r_cislo;
+
+        Regex regex = new Regex(@"^\d{6}\/\d{4}$");
+
+        public string RodneCislo
+        {
+
+            get
+            {
+                return r_cislo;
+            }
+            set
+            {
+                r_cislo = regex.IsMatch(value) ? value : throw new FormatException("špatný formát rodného čísla");
+            }
+        }
+
+        public Obcan()
+        {
+            Jmeno = "nezname";
+            Prijmeni = "nezname";
+            RodneCislo = "000000/0000";
+        }
+
+        public Obcan(string _rodneCislo, string _jmeno, string _prijmeni) : base(_jmeno, _prijmeni)
+        {
+            RodneCislo = _rodneCislo;
+        }
+    }
+}
